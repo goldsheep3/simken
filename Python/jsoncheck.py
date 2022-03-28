7e28760236e4ab60bb776d23af46c0ee38515e70
三金羊Sheep <gold_sheep_3@163.com>
2022-03-28 23:20:06 +0800

    千辛万苦搞定了python的路径问题

---
+++ Python/jsoncheck.py
@@ -0,0 +1,7 @@
+import json
+
+with open("UserData/apps.json", encoding="UTF-8") as f_obj :
+    apps = json.load(f_obj)
+with open("UserData/frames.json", encoding="UTF-8") as f_obj :
+    frames = json.load(f_obj)
+
