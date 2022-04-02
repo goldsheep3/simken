0ae745eb4bcda0e3b0f7193e4cc9d106e59bc4c7
三金羊Sheep <gold_sheep_3@163.com>
2022-04-02 18:22:54 +0800

    重构工程格式

--- jsoncheck.py
+++ jsoncheck.py
@@ -4,4 +4,3 @@ with open("UserData/apps.json", encoding="UTF-8") as f_obj :
     apps = json.load(f_obj)
 with open("UserData/frames.json", encoding="UTF-8") as f_obj :
     frames = json.load(f_obj)
-
